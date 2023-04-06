c58a9873948f5798fa1fec48aae5a3c36790d7bd
Ahamed N <77161518+snappedrat@users.noreply.github.com>
2023-04-06 15:23:09 +0530

    summary

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -2,12 +2,15 @@ const jwt = require('jsonwebtoken');
 
 function verifyJWT(req, res, next) {
   const token = req.headers.authorization;
+  console.log(token)
   if (!token) {
+    console.log('No token provided');
     return res.status(401).json({ message: 'No token provided' });
   }
 
-  jwt.verify(token, process.env.RANE, (err, decodedToken) => {
+  jwt.verify(token, process.env.secret, (err, decodedToken) => {
     if (err) {
+      console.log('Failed to authenticate token')
       return res.status(403).json({ message: 'Failed to authenticate token' });
     }
     req.user = decodedToken;
